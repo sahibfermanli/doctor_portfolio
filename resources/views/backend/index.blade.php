045a717d15119ecfe41b887ad45c457b679e6e49
Sahib Fermanli <Sahib.Fermanli@asg-groundhandling.az>
2021-12-20 00:02:48 +0400

    Production

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -20,7 +20,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 {{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">--}}
 <!-- Styles -->
-    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin.css?ver=0.1.0') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 </head>
 
@@ -32,7 +32,7 @@
     <my-app></my-app>
 </div>
 
-<script src="{{ asset('js/admin.js') }}"></script>
+<script src="{{ asset('js/admin.js?ver=0.1.0') }}"></script>
 </body>
 
 </html>
